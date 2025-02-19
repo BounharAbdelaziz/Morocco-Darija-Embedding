de9004431c16f5235e64e4250494a72eb8123c2c
BounharAbdelaziz <bounhar.abdelaziz@gmail.com>
2025-02-19 20:41:08 +0100

    multi-dataset training with different objectives (mnrl and cosent)

--- sentence_embedding/dynamic_embeddings.py
+++ sentence_embedding/dynamic_embeddings.py
@@ -7,9 +7,22 @@ from sentence_transformers import (
     SentenceTransformerTrainingArguments,
     SentenceTransformerModelCardData,
 )
-from sentence_transformers.losses import MatryoshkaLoss, MultipleNegativesRankingLoss
-from sentence_transformers.training_args import BatchSamplers, MultiDatasetBatchSamplers
-from sentence_transformers.evaluation import TripletEvaluator
+from sentence_transformers.training_args import (
+    BatchSamplers, 
+    MultiDatasetBatchSamplers,
+)
+
+from sentence_transformers.losses import (
+    CoSENTLoss, 
+    MultipleNegativesRankingLoss,
+    MatryoshkaLoss,
+)
+
+from sentence_transformers.evaluation import (
+    TripletEvaluator,
+    TranslationEvaluator,
+    EmbeddingSimilarityEvaluator,
+)
 
 logging.basicConfig(
     format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO
@@ -31,42 +44,79 @@ def set_seed(seed):
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
         
-def load_train_eval_datasets(dataset_path):
-    """
-    Either load the train and eval datasets from disk or load them from the datasets library & save them to disk.
-    Upon saving to disk, we quit() to ensure that the datasets are not loaded into memory before training.
-    """
+def load_train_eval_datasets(train_dataset, bench_dataset="atlasia/Morocco-Darija-Sentence-Embedding-Benchmark"):
+    """ Load and prepare training and evaluation datasets. """
 
     print("Loading dataset...")
-    dataset_1 = load_dataset(dataset_path, split="train")
-    dataset_1 = dataset_1.map(
+    
+    ################################# Triplet dataset #################################
+    
+    dataset_triplet = load_dataset(train_dataset, "triplet", split="train")
+    dataset_triplet = dataset_triplet.map(
         lambda x: {"anchor": x["anchor"], "positive": x["positive"], "negative": x["negative"]}, 
         remove_columns=["original_source", "translation_model", "metadata"]
     )
-    dataset_dict_1 = dataset_1.train_test_split(test_size=250, seed=12)
-    train_dataset_1: Dataset = dataset_dict_1["train"]
-    eval_dataset_1: Dataset = dataset_dict_1["test"]
+    dataset_dict_triplet = dataset_triplet.train_test_split(test_size=250, seed=12)
+    train_dataset_triplet: Dataset = dataset_dict_triplet["train"]
+    eval_dataset_triplet: Dataset = dataset_dict_triplet["test"]
     
-    dataset_2 = load_dataset("BounharAbdelaziz/Terjman-v2-English-Darija-Dataset-350K", split="train")
-    dataset_2 = dataset_2.map(
-        lambda x: {"english": x["english"], "non_english": x["darija_Arab"]}, 
-        remove_columns=["darija_Latn", "darija_Arab", "dataset_source", "id", "role", "darija_tokens", "subtopic", "topic", "annotator_dialect"]
+    ################################# Negation Triplet dataset #################################
+    
+    dataset_negation_triplet = load_dataset(train_dataset, "negation-triplet", split="train")
+    dataset_negation_triplet = dataset_negation_triplet.map(
+        lambda x: {"anchor": x["anchor"], "entailment": x["entailment"], "negative": x["negative"]}, 
+        remove_columns=["original_source", "translation_model", "metadata"]
     )
-    dataset_dict_2 = dataset_2.train_test_split(test_size=250, seed=12)
-    train_dataset_2: Dataset = dataset_dict_2["train"]
-    eval_dataset_2: Dataset = dataset_dict_2["test"]
+    dataset_dict_negation_triplet = dataset_negation_triplet.train_test_split(test_size=250, seed=12)
+    train_dataset_negation_triplet: Dataset = dataset_dict_negation_triplet["train"]
+    eval_dataset_negation_triplet: Dataset = dataset_dict_negation_triplet["test"]
     
+    ################################# Pair Score dataset #################################
+    
+    dataset_pair_score = load_dataset(train_dataset, "pair-score", split="train") #.shuffle(seed=12).select(range(75_00))
+    dataset_pair_score = dataset_pair_score.map(
+        lambda x: {"sentence1": x["sentence1"], "sentence2": x["sentence2"], "score": x["score"]}, 
+        remove_columns=["original_source", "translation_model", "metadata"]
+    )
+    dataset_dict_pair_score = dataset_pair_score.train_test_split(test_size=250, seed=12)
+    train_dataset_pair_score: Dataset = dataset_dict_pair_score["train"]
+    eval_dataset_pair_score: Dataset = dataset_dict_pair_score["test"]
+    
+    ################################# (English, Non english) dataset #################################
+    
+    dataset_english_non_english = load_dataset(train_dataset, "english-non_english", split="train")
+    dataset_english_non_english = dataset_english_non_english.map(
+        lambda x: {"english": x["english"], "non_english": x["non_english"]}, 
+        remove_columns=["original_source"]
+    )
+    dataset_dict_english_non_english = dataset_english_non_english.train_test_split(test_size=250, seed=12)
+    train_dataset_english_non_english: Dataset = dataset_dict_english_non_english["train"]
+    eval_dataset_english_non_english: Dataset = dataset_dict_english_non_english["test"]
+    
+    ############################### AtlasIA Sentence Embedding Benchmark ##############################
+    
+    # already prepared
+    atlasia_sent_embd_bench = load_dataset(bench_dataset, split="test")
+    
+    ##################################################################################################
+    
+    # combine all datasets
     train_dataset = DatasetDict({
-        "anchor_trans": train_dataset_1,
-        "terjman_v2_350K": train_dataset_2,
+        "triplet": train_dataset_triplet,
+        "negation_triplet": train_dataset_negation_triplet,
+        "pair_score": train_dataset_pair_score,
+        "english_non_english": train_dataset_english_non_english,
     })
     
     eval_dataset = DatasetDict({
-        "anchor_trans": eval_dataset_1,
-        "terjman_v2_350K": eval_dataset_2,
+        "triplet": eval_dataset_triplet,
+        "negation_triplet": eval_dataset_negation_triplet,
+        "pair_score": eval_dataset_pair_score,
+        "english_non_english": eval_dataset_english_non_english,
+        "atlasia_sent_embd_bench": atlasia_sent_embd_bench,
     })
    
-    print("Loaded dataset.")
+    print("[INFO] Loaded train and eval datasets.")
     
     return train_dataset, eval_dataset
 
@@ -101,6 +151,9 @@ if __name__ == "__main__":
 
     # Training data path
     TRAIN_DATA_PATH = config['DATASET_PATH']
+    
+    # Benchmark data path
+    BENCH_DATASET_PATH = config['BENCH_DATASET_PATH']
     
     # base model path
     MODEL_PATH = config['BASE_MODEL']
@@ -154,15 +207,33 @@ if __name__ == "__main__":
     )
 
     # 3. Set up training & evaluation datasets - each dataset is trained with MNRL (with MRL)
-    train_dataset, eval_dataset = load_train_eval_datasets(TRAIN_DATA_PATH)
+    train_dataset, eval_dataset = load_train_eval_datasets(
+        train_dataset=TRAIN_DATA_PATH, 
+        bench_dataset=BENCH_DATASET_PATH
+    )
     
     print(f'train_dataset: {train_dataset}')
     print(f'eval_dataset: {eval_dataset}')
-
-    # 4. Define a loss function
-    loss = MultipleNegativesRankingLoss(model)
-    loss = MatryoshkaLoss(model, loss, matryoshka_dims=[32, 64, 128, 256, 512, 768])
-
+    
+    # 4. Load several loss functions to train with
+    
+    # (anchor, positive), (anchor, positive, negative)
+    mnrl_loss = MultipleNegativesRankingLoss(model)
+    mnrl_loss = MatryoshkaLoss(model, mnrl_loss, matryoshka_dims=[32, 64, 128, 256, 512, 768])
+    
+    # (sentence_A, sentence_B) + score
+    cosent_loss = CoSENTLoss(model)
+    
+    # Create a mapping with dataset names to loss functions, so the trainer knows which loss to apply where.
+    # Note that you can also just use one loss if all of your training/evaluation datasets use the same loss
+    losses = {
+        "triplet": mnrl_loss,
+        "negation_triplet": mnrl_loss,
+        "pair_score": cosent_loss,
+        "english_non_english": mnrl_loss,
+        "atlasia_sent_embd_bench": cosent_loss, # is not used for trainning but must be included in the losses dict
+    } 
+   
     # 5. Specify training arguments
     training_args = SentenceTransformerTrainingArguments(
         output_dir=MODEL_RUN_SAVE_PATH,
@@ -190,27 +261,73 @@ if __name__ == "__main__":
     )
     
     # 6. (Optional) Create an evaluator & evaluate the base model
-    dev_evaluator = TripletEvaluator(
-        anchors=eval_dataset['anchor_trans']["anchor"],
-        positives=eval_dataset['anchor_trans']["positive"],
-        negatives=eval_dataset['anchor_trans']["negative"],
+    # Evaluators
+    dev_evaluators = []
+
+    ## triplet dataset
+    triplet_evaluator = TripletEvaluator(
+        anchors=eval_dataset['triplet']["anchor"],
+        positives=eval_dataset['triplet']["positive"],
+        negatives=eval_dataset['triplet']["negative"],
         name="triplet-evaluator-dev",
     )
-    dev_evaluator(model)
+    dev_evaluators.append(triplet_evaluator)
+    
+    ## negation triplet dataset
+    negation_triplet_evaluator = TripletEvaluator(
+        anchors=eval_dataset['negation_triplet']["anchor"],
+        positives=eval_dataset['negation_triplet']["entailment"],
+        negatives=eval_dataset['negation_triplet']["negative"],
+        name="negation-triplet-evaluator-dev",
+    )
+    dev_evaluators.append(negation_triplet_evaluator)
+    
+    ## pair score dataset
+    pair_score_evaluator = EmbeddingSimilarityEvaluator(
+        sentences1=eval_dataset['pair_score']["sentence1"],
+        sentences2=eval_dataset['pair_score']["sentence2"],
+        scores=eval_dataset['pair_score']["score"],
+        name="pair-score-evaluator-dev",
+    )
+    dev_evaluators.append(pair_score_evaluator)
+    
+    # English, Non-English dataset
+    translation_evaluator = TranslationEvaluator(
+        source_sentences=eval_dataset['english_non_english']["english"],
+        target_sentences=eval_dataset['english_non_english']["non_english"],
+        name="english_non_english-evaluator-dev",
+    )
+    dev_evaluators.append(translation_evaluator)
+    
+    ## atlasia_sent_embd_bench dataset
+    atlasia_bench_evaluator = EmbeddingSimilarityEvaluator(
+        sentences1=eval_dataset['atlasia_sent_embd_bench']["sentence1"],
+        sentences2=eval_dataset['atlasia_sent_embd_bench']["sentence2"],
+        scores=eval_dataset['atlasia_sent_embd_bench']["score"],
+        name="atlasia_sent_embd_bench-evaluator-dev",
+    )
+    dev_evaluators.append(atlasia_bench_evaluator)
+    
+    # Evaluate the model before training
+    triplet_evaluator(model)
+    negation_triplet_evaluator(model)
+    pair_score_evaluator(model)
+    translation_evaluator(model)
+    atlasia_bench_evaluator(model)
 
-    # 6. Create a trainer & train
+    # 7. Create a trainer & train
     trainer = SentenceTransformerTrainer(
         model=model,
         args=training_args,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
-        loss=loss,
-        evaluator=dev_evaluator,
+        loss=losses,
+        evaluator=dev_evaluators,
     )
     trainer.train()
 
-    # 7. Save the trained model
+    # 8.1 Save the trained model
     model.save_pretrained(MODEL_RUN_SAVE_PATH)
 
-    # 8. Push it to the Hugging Face Hub
+    # 8.2 Push it to the Hugging Face Hub
     model.push_to_hub(MODEL_RUN_SAVE_PATH, private=True)
